472611b8bfd2242a37ec1afb53f1ed35a03f4d39
jacunar <josueally@gmail.com>
2023-10-05 23:30:26 -0600

    Agregado paquete, Agregado video

---
+++ Exercises/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Exercises {
+    public partial class App : Application {
+        public App() {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Exercises/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Exercises; 
+public partial class MainPage : ContentPage {
+    public MainPage() {
+        InitializeComponent();
+    }
+
+    public void ContentPage_Unloaded(object? sender, EventArgs e) {
+        mediaElement.Handler?.DisconnectHandler();
+    }
+}
