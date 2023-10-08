92b0c8b354bf9847d40e65031f30782e038d14be
jacunar <josueally@gmail.com>
2023-10-07 22:51:33 -0600

    Avances en funcionalidad de reproducción de video. Cambio de versión de libreria

--- Exercises/MainPage.xaml.cs
+++ Exercises/MainPage.xaml.cs
@@ -1,11 +1,30 @@
-﻿namespace Exercises; 
+﻿using CommunityToolkit.Maui.Views;
+
+namespace Exercises; 
 public partial class MainPage : ContentPage {
     public MainPage() {
         InitializeComponent();
         //DeviceDisplay.Current.KeepScreenOn = true;
     }
 
-    public void ContentPage_Unloaded(object? sender, EventArgs e) {
-        mediaElement.Handler?.DisconnectHandler();
+    async void OnShowVideoLibraryClicked(object sender, EventArgs e) {
+        //try {
+        //    Button button = sender as Button;
+        //    button.IsEnabled = false;
+
+        //    var pickedVideo = await MediaPicker.PickVideoAsync();
+        //    if (!string.IsNullOrWhiteSpace(pickedVideo?.FileName)) {
+        //        video.Source = MediaSource.FromFile(pickedVideo.FileName);
+        //        video.Play();
+        //    }
+
+        //    button.IsEnabled = true;
+        //} catch (Exception ex) {
+        //    await DisplayAlert("Error", ex.Message, "Ok");
+        //}        
+    }
+
+    public void ContentPage_Unloaded(object sender, EventArgs e) {
+        video.Handler?.DisconnectHandler();
     }
 }
